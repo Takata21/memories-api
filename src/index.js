5cc33d991a8c51193df27b70244aa37a9b119d87
TAKATA <takata211011@mail.com>
2023-01-21 23:57:02 -0500

    add file Upload and errors middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import express from 'npm:express'
+import fileUpload from 'npm:express-fileupload'
 import morgan from 'npm:morgan'
 import { config } from 'https://deno.land/std@0.173.0/dotenv/mod.ts'
+import authRoutes from './routes/Auth.routes.js'
 import './config/mongoose.js'
 
 const app = express()
 app.use(express.json())
-// app.use(morgan('dev'))
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.urlencoded({ extended: false }))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+  })
+)
+app.use('/auth', authRoutes)
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    status: err.status || 500,
+    message: err.message || 'Server error',
+  })
 })
 app.listen(3000, () => {
   console.log('server on port: 3000')
