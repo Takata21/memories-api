c5825bda5f0f4872b3413665367c9db8b5ce1920
TAKATA <takata211011@mail.com>
2023-01-21 23:51:51 -0500

    add cloudinary and jwt env variables

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
 import 'https://deno.land/std@0.173.0/dotenv/load.ts'
 
 export const MONGODB_URL = Deno.env.get('MONGODB_URL')
+export const JWT_SECRET = Deno.env.get('JWT_SECRET')
+export const CLOUDINARY_CLOUD_NAME = Deno.env.get('CLOUDINARY_CLOUD_NAME')
+export const CLOUDINARY_API_KEY = Deno.env.get('CLOUDINARY_API_KEY')
+export const CLOUDINARY_API_SECRET = Deno.env.get('CLOUDINARY_API_SECRET')
