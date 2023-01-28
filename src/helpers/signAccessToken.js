9c88e9ea595bc2e04a34430cfbfe240763527048
Manuel Takata <takata211011@gmail.com>
2023-01-27 21:30:24 -0500

    add token verification and decode

--- src/helpers/signAccessToken.js
+++ src/helpers/signAccessToken.js
@@ -1,6 +1,4 @@
-import { create } from 'https://deno.land/x/djwt@v2.8/mod.ts'
-import { JWT_SECRET } from '../config.js'
-console.log(JWT_SECRET)
+import { create, verify, decode } from 'https://deno.land/x/djwt@v2.8/mod.ts'
 const key = await crypto.subtle.generateKey(
   { name: 'HMAC', hash: 'SHA-512' },
   true,
@@ -11,6 +9,23 @@ export const signAccessToken = (userId) => {
     if (!userId) {
       reject(new Error('userId is required'))
     }
+    console.log(key)
     resolve(create({ alg: 'HS512', typ: 'JWT' }, { id: userId }, key))
   })
+}
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.headers.authorization
+  console.log(token)
+  if (!token) res.status(401).send({ message: 'Unauthorized' })
+  try {
+    const [header, payload, signature] = decode(token)
+    // const payload = await verify(token, key)
+    console.log(payload)
+    // req.userId = payload.id
+    next()
+  } catch (error) {
+    res.status(401).send({ message: 'Unauthorized' })
+    console.log(error)
+  }
 }
