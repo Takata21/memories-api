fb82bf4a4b7d6f81a4257c4713e6ad14d0997326
Manuel Takata <takata211011@gmail.com>
2023-01-27 21:31:46 -0500

    add middleware to verify token

--- src/routes/Auth.routes.js
+++ src/routes/Auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'npm:express'
 import { login, profile, register } from '../controllers/auth.controllers.js'
+import { verifyToken } from '../helpers/signAccessToken.js'
 
 const router = Router()
 
 router.post('/login', login)
 router.post('/register', register)
-router.get('/profile', profile)
+router.get('/profile', verifyToken, profile)
 
 export default router
